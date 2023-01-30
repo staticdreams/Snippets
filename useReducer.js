e9cf9f1fd30984b010c3315911492ae089e4df3a
Peter Tikhomirov <peter.tikhomirov@gmail.com>
2023-01-30 19:41:11 +0100

    Update useReducer.js

--- useReducer.js
+++ useReducer.js
@@ -1,2 +1,21 @@
+const [event, updateEvent] = useReducer((state, action) => {
+    const newEvent = {...state}
 
-
+    switch (action.type) {
+      case 'setTitle':
+        newEvent.title = action.title;
+        break;
+      // More actions...
+    }
+    return newEvent
+  }, { title: '', description: '', attendees: [] })
+  
+  
+  //-------------------
+  
+  return <>
+    <input value={event.title} 
+      onChange={e => updateEvent({ type: 'setTitle', title: 'Hello' }) } />
+    {/* ... */}
+  </>
+  
